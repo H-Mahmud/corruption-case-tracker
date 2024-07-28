da7ad21befbf1467eae84b2b1ee4e94469d61030
Mahmud <me@imahmud.com>
2024-07-28 09:54:34 +0600

    Plugin init

---
+++ cct.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Plugin Name:       Corruption Case Tracker
+ * Plugin URI:        https://github.com/H-Mahmud/corruption-case-tracker/
+ * Description:       The Corruption Case Tracker plugin is an essential tool designed to create an online platform that tracks and presents data on corruption cases in Liberia. This plugin makes vital information accessible to anti-corruption advocates, donor partners, and stakeholders, aiming to bridge the data gap and support integrity-related advocacy.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Dream Developer
+ * Author URI:        https://dreamdeveloper.org/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       cct
+ * Domain Path:       /languages
+ * Requires Plugins:  advanced-custom-fields
+ */
