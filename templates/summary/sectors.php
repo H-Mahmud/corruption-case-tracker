f31b323fdcfc9c27bcb90b267ab40288ea78e6f8
Mahmud <me@imahmud.com>
2024-08-23 17:02:20 +0600

    Add sectors summary

---
+++ templates/summary/sectors.php
@@ -0,0 +1,36 @@
+<?php defined('ABSPATH') || exit();
+
+if (!isset($_GET['summary_by']) || $_GET['summary_by'] !== 'sectors')
+    return;
+?>
+
+<canvas id="sectorChart" style="width:100%;max-width:100%"></canvas>
+
+<script>
+
+    <?php
+    $case_status = CCT_Utils::get_field_options('sector_of_the_case');
+
+
+    $data = [];
+    foreach ($case_status as $key => $value) {
+        $data[$value] = CCT_Utils::get_case_count_by_meta('sector_of_the_case', $key);
+    }
+
+    ?>
+    const data = {
+        datasets: [{
+            label: '',
+            data: <?php echo json_encode($data); ?>,
+        }]
+    };
+
+    const config = {
+        type: 'bar',
+        data: data,
+
+    };
+
+
+    new Chart("sectorChart", config);
+</script>
