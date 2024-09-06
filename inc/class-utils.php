0d570a71329b7f04626c3ee20924ac8d073a2a2c
Mahmud <me@imahmud.com>
2024-09-06 10:55:07 +0600

    Fix bugs

--- inc/class-utils.php
+++ inc/class-utils.php
@@ -150,6 +150,69 @@ if (!class_exists('CCT_Utils')) {
         }
 
         /**
+         * Update case status date on custom date table
+         * @param mixed $post_id case id
+         * @param mixed $case_status case status
+         * @param mixed $start_date case start date 
+         * @param mixed $end_date case end date
+         * @return bool|int
+         */
+        public static function update_case_status_date($post_id, $case_status, $start_date, $end_date)
+        {
+            global $wpdb;
+            $table_name = $wpdb->prefix . 'case_date_data';
+
+            $data = array(
+                'post_id' => $post_id,
+                'start_date' => $start_date,
+                'end_date' => $end_date,
+                'status' => $case_status
+            );
+
+            $format = array('%d', '%s', '%s', '%s');
+
+            $results = $wpdb->get_row($wpdb->prepare("SELECT id FROM $table_name WHERE post_id=%d", $post_id));
+
+            if ($results)
+                $data_id = $wpdb->update($table_name, $data, array("id" => $results->id));
+            else
+                $data_id = $wpdb->insert($table_name, $data, $format);
+            return $data_id;
+        }
+
+
+        /**
+         * Update Delay case status date on custom date table
+         * @param mixed $post_id case id
+         * @param mixed $case_status case status
+         * @param mixed $start_date case start date 
+         * @param mixed $end_date case end date
+         * @return bool|int
+         */
+        public static function update_delay_case_status_date($post_id, $case_status, $start_date, $end_date)
+        {
+            global $wpdb;
+            $table_name = $wpdb->prefix . 'case_date_data';
+
+            $data = array(
+                'post_id' => $post_id,
+                'start_date' => $start_date,
+                'end_date' => $end_date,
+                'status' => $case_status
+            );
+
+            $format = array('%d', '%s', '%s', '%s');
+
+            $results = $wpdb->get_row($wpdb->prepare("SELECT id FROM $table_name WHERE post_id=%d AND status=%s", $post_id, $case_status));
+
+            if ($results)
+                $data_id = $wpdb->update($table_name, $data, ["id" => $results->id]);
+            else
+                $data_id = $wpdb->insert($table_name, $data, $format);
+            return $data_id;
+        }
+
+        /**
          * Insert delay status date field
          * @param mixed $post_id case id
          * @param mixed $case_status delay case status
@@ -160,15 +223,13 @@ if (!class_exists('CCT_Utils')) {
          */
         public static function delay_status_date_insert($post_id, $case_status, $is_delay, $start, $end)
         {
-            $case_status = $_POST['acf'][$is_delay];
-
             if (isset($_POST['acf'][$is_delay]) && $_POST['acf'][$is_delay]) {
                 $start_date_obj = DateTime::createFromFormat('Ymd', $_POST['acf'][$start]);
                 $start_date = $start_date_obj->format('Y-m-j');
 
                 $end_date_obj = DateTime::createFromFormat('Ymd', $_POST['acf'][$end]);
                 $end_date = $end_date_obj->format('Y-m-j');
-                // cct_update_case_status_date($post_id, $case_status, $start_date, $end_date);
+                return CCT_Utils::update_delay_case_status_date($post_id, $case_status, $start_date, $end_date);
             }
 
             return 0;
