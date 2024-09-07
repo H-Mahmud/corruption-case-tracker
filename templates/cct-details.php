56b9da9ef53cf974bf77d8627be370baeb09b921
Mahmud <me@imahmud.com>
2024-09-07 15:40:19 +0600

    Show delay duration statuses

--- templates/cct-details.php
+++ templates/cct-details.php
@@ -27,12 +27,13 @@
     <?php }
     ; ?>
     <?php
-    if (get_field('is_the_case_delayed')) { ?>
+    if (get_delay_duration_statuses()) {
+        ?>
         <tr>
             <th>Delay Duration</th>
             <td class="delay-status">
                 <?php
-                include_once(__DIR__ . '/delay-duration.php');
+                echo get_delay_duration_statuses();
                 ?>
             </td>
         </tr>
@@ -81,3 +82,122 @@
         <td> <?php the_field('nature_of_the_case'); ?> </td>
     </tr>
 </table>
+
+
+<?php
+
+function get_delay_duration_statuses()
+{
+    $fields = [
+        [
+            'status' => 'alleged',
+            'is_delay' => 'field_66da7dac2cb54',
+            'start_date' => 'field_66da7df92cb55',
+            'end_date' => 'field_66da7e5e2cb56'
+        ],
+        [
+            'status' => 'pending_investigation',
+            'is_delay' => 'field_66da7ecb9a08b',
+            'start_date' => 'field_66da7efa9a08c',
+            'end_date' => 'field_66da7f1c9a08d'
+        ],
+        [
+            'status' => 'under_investigation',
+            'is_delay' => 'field_66dc0c10a8fe2',
+            'start_date' => 'field_66dc0c25a8fe3',
+            'end_date' => 'field_66dc0c97a8fe4'
+        ],
+        [
+            'status' => 'indictment_drawn',
+            'is_delay' => 'field_66dc0cc2f5502',
+            'start_date' => 'field_66dc0d1df5503',
+            'end_date' => 'field_66dc0d44f5504'
+        ],
+        [
+            'status' => 'in_court',
+            'is_delay' => 'field_66dc0d8b5c644',
+            'start_date' => 'field_66dc0dae5c645',
+            'end_date' => 'field_66dc0dc95c646'
+        ],
+        [
+            'status' => 'concluded',
+            'is_delay' => 'field_66dc0e2f83dc0',
+            'start_date' => 'field_66dc0e4383dc1',
+            'end_date' => 'field_66dc0e5e83dc2'
+        ],
+        [
+            'status' => 'concluded_conviction',
+            'is_delay' => 'field_66dc0e7faa64d',
+            'start_date' => 'field_66dc0e8eaa64e',
+            'end_date' => 'field_66dc0ea6aa64f'
+        ],
+        [
+            'status' => 'concluded_not_guilty',
+            'is_delay' => 'field_66dc0ec3bc7f2',
+            'start_date' => 'field_66dc0edabc7f3',
+            'end_date' => 'field_66dc0efabc7f4'
+        ],
+        [
+            'status' => 'on_appeal_to_supreme_court',
+            'is_delay' => 'field_66dc0f2017b31',
+            'start_date' => 'field_66dc0f3417b32',
+            'end_date' => 'field_66dc0f4b17b33'
+        ]
+    ];
+
+
+    $status_obj = get_field_object('case_status');
+
+
+    $badged = false;
+    foreach ($fields as $field) {
+
+        if (get_field($field['is_delay'])) {
+            $bg_color = CCT_Utils::get_status_color($field['status']) . '33';
+
+            $start_date_key = get_field_object($field['start_date'])['_name'];
+            $end_date_key = get_field_object($field['end_date'])['_name'];
+
+            $start = get_field($start_date_key);
+            $end = get_field($end_date_key);
+
+            // Convert the dates into DateTime objects
+            $startDate = DateTime::createFromFormat('d/m/Y', $start);
+            $endDate = DateTime::createFromFormat('d/m/Y', $end);
+
+            // Calculate the difference between the two dates
+            $interval = $startDate->diff($endDate);
+
+
+            $label = $status_obj['choices'][$field['status']] . ' ' . cct_format_interval($interval);
+            ;
+            $badged .= <<<HTML
+                <span style="background-color: $bg_color;">$label</span>
+            HTML;
+        }
+    }
+    return $badged;
+}
+
+function cct_format_interval($interval)
+{
+    $parts = [];
+
+    if ($interval->y > 0) {
+        $parts[] = $interval->y . ' year' . ($interval->y > 1 ? 's' : '');
+    }
+
+    if ($interval->m > 0) {
+        $parts[] = $interval->m . ' month' . ($interval->m > 1 ? 's' : '');
+    }
+
+    if ($interval->d > 0) {
+        $parts[] = $interval->d . ' day' . ($interval->d > 1 ? 's' : '');
+    }
+
+    if (empty($parts)) {
+        $parts[] = '0 days';
+    }
+
+    return implode(', ', $parts);
+}
