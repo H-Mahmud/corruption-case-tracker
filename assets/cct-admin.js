2c713bc56521028bf50814a23bafff8ea33433f0
Mahmud <me@imahmud.com>
2024-09-25 17:24:11 +0600

    Send csv data mapper ajax request

---
+++ assets/cct-admin.js
@@ -0,0 +1,58 @@
+jQuery(document).ready(function ($) {
+    $("body").on("click", "#insertCsv", function (e) {
+        e.preventDefault();
+
+        var button = $(this);
+
+        var customUploader = wp.media({
+            title: "Insert CSV",
+            library: { type: "text/csv" },
+            button: {
+                text: "Use This CSV",
+            },
+            multiple: false,
+        })
+            .on("select", function () {
+                var attachment = customUploader.state().get("selection").first().toJSON();
+
+                $(button)
+                    .removeClass("button")
+                    .html('<span class="dashicons dashicons-media-document"></span>')
+                    .next().val(attachment.id)
+                    .next().show();
+
+                fetchCSVDataMapper(attachment.id);
+            })
+            .open();
+    });
+
+    // Remove CSV file
+    $("body").on("click", ".remove-csv", function () {
+        $(this).hide().prev().val("").prev().addClass("button").html("Upload CSV");
+        return false;
+    });
+
+    /**
+     * Read CSV Header
+     */
+    function fetchCSVDataMapper(csvId) {
+        $('.import-spinner').addClass('is-active');
+        $.ajax({
+            type: "POST",
+            url: CCT.ajaxUrl,
+            data: {
+                action: "fetch_csv_mapper",
+                nonce: CCT.nonce,
+                csvId: csvId,
+            },
+        }).done(function (response) {
+            $('.form-table').html(response);
+            $('.import-spinner').removeClass('is-active');
+
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            alert('Something went wrong!')
+            $('.import-spinner').removeClass('is-active');
+
+        });
+    }
+});
--- templates/tools/import.php
+++ templates/tools/import.php
@@ -1,1 +1,21 @@
-<h1>Import</h1>
+<form>
+    <div class="import-container">
+        <h1 class="title">Import Cases From CSV</h1>
+        <hr>
+        <form id="import-form">
+            <table class="form-table">
+                <tr>
+                    <th><label for="csvFile">Insert A CSV File</label></th>
+                    <td>
+                        <div>
+                            <button class="button" id="insertCsv">Insert CSV</button>
+                            <input type="hidden" required name="csv-file" id="csvFile" value="" />
+                            <a href="#" class="remove-csv">Remove CSV</a>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+            <div class="spinner import-spinner"></div>
+        </form>
+    </div>
+</form>
